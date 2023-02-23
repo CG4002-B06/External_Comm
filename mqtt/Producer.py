408783884d5f9eec6bb56008bf279b8896c390bb
Siyuan Cheng <siyuanc6794@gmail.com>
2023-02-23 19:27:35 +0800

    modify default max inflight

--- mqtt/Producer.py
+++ mqtt/Producer.py
@@ -19,11 +19,12 @@ class Producer(Thread):
         self.client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
         self.client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
         self.client.username_pw_set(mq_username, mq_password)
+        self.client.max_inflight_messages_set(mqtt_constant.MAX_INFLIGHT)
         self.client.connect(mqtt_constant.MESSAGE_QUEUE_URL, 8883)
 
     def run(self):
         print("start publishing data to HiveMQ")
         while True:
             action = self.queue.get()
-            self.client.publish(mqtt_constant.PUBLISH_TOPIC, payload=action, qos=2)
+            self.client.publish(mqtt_constant.PUBLISH_TOPIC, payload=action, qos=1)
             print("published message: " + str(action))
