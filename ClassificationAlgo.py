9766a355f4d65773c70273dc03f0c9a1359b39c9
SvethaMahadevan <69336238+SvethaMahadevan@users.noreply.github.com>
2023-03-06 18:02:48 +0800

    Add files via upload

---
+++ ClassificationAlgo.py
@@ -0,0 +1,28 @@
+import numpy
+def classifyMove(flattenedData)
+    overlay = Overlay('design_1_wrapper.bit')
+    dma = overlay.axi_dma_0
+
+    input_buffer = allocate(shape=(180,), dtype=np.float32)
+    output_buffer = allocate(shape=(1,), dtype=np.float32)
+
+    inputData=flattenedData
+
+    for x, n in enumerate(inputData):
+        input_buffer[x] = n
+    dma.sendchannel.transfer(input_buffer)
+    dma.recvchannel.transfer(output_buffer)
+    dma.sendchannel.wait()
+    dma.recvchannel.wait()
+
+    return action 
+
+def getSlidingWindows(readings, window_size, 1):
+    result = []
+    for i in range(0, len(readings) - window_size + 1, 1):
+        result.append(readings[i:i + window_size])
+    return result
+    
+def  flattenTheWindows()
+    sliding_windows = MyClass.getSlidingWindows(data, window_size, 1)
+    return [np.flatten(x) for x in sliding_windows]
