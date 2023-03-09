aa058f34a824f10b0c243454beaa004caac32deb
Siyuan Cheng <siyuanc6794@gmail.com>
2023-03-09 14:38:15 +0800

    Update test_relay_client.py

--- test_relay_client.py
+++ test_relay_client.py
@@ -11,7 +11,7 @@ data_queue, response_queue = Queue(), Queue()
 if __name__ == "__main__":
     t = Thread(target=run, args=(data_queue, response_queue, event)).start()
     while True:
-        data = struct.pack(VEST_FORMAT, bytes("D",'utf8'), bytes("V1", 'utf8'), False)
+        data = struct.pack(VEST_FORMAT, bytes("D",'utf8'), bytes("V1", 'utf8'), True)
         data_queue.put(data)
         time.sleep(0.4)
 
