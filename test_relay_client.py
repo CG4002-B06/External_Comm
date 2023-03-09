b6f1d203dedf93b2616bb2d173fca3137f9044ca
Siyuan Cheng <siyuanc6794@gmail.com>
2023-03-09 20:02:23 +0800

    Update test_relay_client.py

--- test_relay_client.py
+++ test_relay_client.py
@@ -3,18 +3,31 @@ import time
 from queue import Queue
 from threading import Event, Thread
 from relay.relay_client import *
+import random
 
 VEST_FORMAT = '<c2s?'
+GLOVES_FORMAT = '<c3s6h'
 event = Event()
 data_queue, response_queue = Queue(), Queue()
+
 
 if __name__ == "__main__":
     t = Thread(target=run, args=(data_queue, response_queue, event)).start()
     while True:
-        data = struct.pack(VEST_FORMAT, bytes("D",'utf8'), bytes("V1", 'utf8'), True)
+
+        data = struct.pack(
+            GLOVES_FORMAT,
+            bytes("D",'utf8'),
+            bytes("GL1", 'utf8'),
+            random.randint(0, 10000),
+            random.randint(0, 10000),
+            random.randint(0, 10000),
+            random.randint(0, 10000),
+            random.randint(0, 10000),
+            random.randint(0, 10000)
+        )
         data_queue.put(data)
-        print("sent")
-        time.sleep(0.4)
+        time.sleep(0.1)
 
 
 
