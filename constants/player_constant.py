d370e51d3e84247a2ed5c26365dbbba604f6e443
kishorkumar11 <h.kishorkumar7@gmail.com>
2023-03-21 21:10:37 +0800

    Updated player_constant.py

--- constants/player_constant.py
+++ constants/player_constant.py
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
 RELOAD_ERROR_MESSAGE = "Warning! \n\n Unable to reload as you still have ammo"
 SHOOT_ERROR_MESSAGE = "Warning! \n\n You are out of ammo"
 GRENADE_ERROR_MESSAGE = "Warning! \n\n You are out of grenades"
-SHIELD_ERROR_MESSAGE = "Warning! \n\n  You are out of shields"
+SHIELD_ERROR_MESSAGE = "Warning! \n\n You are out of shields"
 SHIELD_COOLDOWN_MESSAGE = "Warning! \n\n Shield on cooldown"
-TARGET_VIEW_ERROR_MESSAGE = "Miss! \n\n Target not found."
-=======
-RELOAD_ERROR_MESSAGE = "Warning! Unable to reload as you still have ammo"
-SHOOT_ERROR_MESSAGE = "Warning! You are out of ammo"
-GRENADE_ERROR_MESSAGE = "Warning! You are out of grenades"
-SHIELD_ERROR_MESSAGE = "Warning! You are out of shields"
-SHIELD_COOLDOWN_MESSAGE = "Warning! Shield on cooldown"
 TARGET_VIEW_ERROR_MESSAGE = "Miss! Target not found."
 
-WAIT_SENSOR_INIT_MESSAGE = "Please don't move the gloves. Wait " \
-                           "for sensors to initialize"
-INIT_COMPLETE_MSG = "Sensors have initialized. Enjoy shooting!"
-REDO_ACTION_MSG = "Oops! Not recognize your action. Pls redo it!"
-SENSOR_DISCONNECT_MSG = "Oops! You are disconnected with the relay node. " \
-                        "Pls move closer."
-SENSOR_RECONNECT_MSG = "Yeah! Connection is back. Resume the game."
->>>>>>> f0ec6cdd9797b496c1838532699e6a2145c21fe9
+WAIT_SENSOR_INIT_MESSAGE = "DONT MOVE GLOVE! \n SENSORS ARE INITIALISING..."
+INIT_COMPLETE_MSG = "SENSORS HAVE BEEN INITIALISED \n ENJOY SHOOTING!"
+REDO_ACTION_MSG = "ACTION UNDETECTED! \n REDO ACTION"
+SENSOR_DISCONNECT_MSG = "CONNECTION LOST \n GET CLOSER TO THE RELAY NODE"
+SENSOR_RECONNECT_MSG = "CONNECTION RE-ESTABLISHED"
